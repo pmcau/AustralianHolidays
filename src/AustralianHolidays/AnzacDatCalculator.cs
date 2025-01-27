2e8c357d0697f22682535afd4522736ae572d35f
Simon Cropp <simon.cropp@gmail.com>
2025-01-28 10:13:54 +1100

    Update AnzacDatCalculator.cs

--- src/AustralianHolidays/AnzacDatCalculator.cs
+++ src/AustralianHolidays/AnzacDatCalculator.cs
@@ -2,7 +2,11 @@
 
 public static class AnzacDatCalculator
 {
-    public static bool IsAnzacDay(this Date date)
+    public static bool IsAnzacDay(this Date date) =>
+        date is { Month: 4, Day: 25 } &&
+        date.IsWeekday();
+
+    public static bool IsAnzacDay2(this Date date)
     {
         if (date.Month == 4)
         {
